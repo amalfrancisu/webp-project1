dd5769c451e6840c83e9c9df91605ba60231c0a7
Arjun BJ <47909735+Arjun-B-J@users.noreply.github.com>
2021-11-08 16:10:35 +0530

    Fixed a small Bug
    Fixed a small Bug

--- signin.php
+++ signin.php
@@ -97,7 +97,7 @@ if (isset($_POST['login'])){
 					document.getElementById('message1').style.color = 'lightcoral';
 				</script>
 			</div>
-			<input type="password" id="password" name="password" pattern="(?=.*\d).{8,}" title="Must contain at least one number and at least 8 or more characters" required />
+			<input type="password" id="password" placeholder="password" name="password" pattern="(?=.*\d).{8,}" title="Must contain at least one number and at least 8 or more characters" required />
 			<input name="dob" type="date" placeholder="Date Of Birth" />
 			<input name="institute" type="text" placeholder="Institute Name" />
 			<button type="submit" name="create">Sign Up</button>
